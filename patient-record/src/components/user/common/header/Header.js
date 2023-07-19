da5e1d1feb03bd2787ec00be2fd4d6d0da772d7b
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-07-19 18:56:40 +0400

    07/19

--- patient-record/src/components/user/common/header/Header.js
+++ patient-record/src/components/user/common/header/Header.js
@@ -1,17 +1,50 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import logo from '../../../../assets/img/lifecenter.png'
+import { Button, Container, Form, Nav, NavDropdown, Navbar, Row } from 'react-bootstrap';
 
 const Header = () => {
   return (
-      <div>
+      <div className='menubar'>
           <Container className="p-0 fixed-top">
               <div className='header'>
-                  <div className='logo'>
-                      
-                  </div>
-                  <div className='menus'>
-                      
-                  </div>
+              <Navbar expand="lg">
+            <Container className="ps-0">
+              <Row>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                
+                <Nav className="me-auto">
+                <Navbar.Brand href="#home">
+                  <img
+                    src={logo}
+                    className="d-inline-block align-top"
+                    alt="Life Center"
+                  />
+                </Navbar.Brand>
+                  <NavDropdown title="Link" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Form className="d-flex">
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                  </Form>
+                  </Nav>
+                </Navbar.Collapse>
+                </Row>
+              </Container>
+            </Navbar>
               </div>
           </Container>
     </div>
