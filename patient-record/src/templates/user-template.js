18a10b964ef072a4c728e814f0e05400d3f9a804
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-07-31 23:57:14 +0400

    07/31

--- patient-record/src/templates/user-template.js
+++ patient-record/src/templates/user-template.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Header from '../components/user/common/header/header.js'
-import Footer from '../components/user/common/footer/footer.js'
 
 const UserTemplate = ({children}) => {
   return (
     <div>
       <Header/>
         {children}
-      <Footer/>
     </div>
   )
 }
