2e9d13993778b7411ba6181210151b6cddce1fd2
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-08-28 18:39:59 +0400

    08/28 v3

--- patient-record/src/components/user/patient/new-patient.js
+++ patient-record/src/components/user/patient/new-patient.js
@@ -29,7 +29,11 @@ const NewPatient = () => {
             <Form.Check
               label="Kadın"
                   />
-            </Col>
+          </Col>
+          
+
+
+
             <Col xl={5} lg={5} md={4} sm={4}>
               <Row className="row-cols-1 row-cols-md-1">
                 <Form.Group as={Col} className="mb-3">
@@ -47,8 +51,20 @@ const NewPatient = () => {
                     type="text"
                   />
                 </Form.Group>
+            </Row>
+            <Row>
+                <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                   type="email"
+                 />
+                </Form.Group>
               </Row>
           </Col>
+
+
+
+
           <Col xl={5} lg={5} md={4} sm={4}>
               <Row className="row-cols-1 row-cols-md-1">
                 <Form.Group as={Col} className="mb-3">
@@ -69,17 +85,64 @@ const NewPatient = () => {
                 />
               </Form.Group>
             </Row>
+            <Row>
+              <Form.Group>
+              <Form.Label>Telefon numarası</Form.Label>
+              <Form.Control
+                 type="text"
+                 as={ReactInputMask}
+                 mask="(999)-999-9999"
+              />
+              </Form.Group>
+              
+            </Row>
           </Col>
+
+
+
+
+
           <Col xl={12} lg={12} md={12} sm={12}>
+              <Row className="row-cols-1 row-cols-md-1">
+                <Form.Group as={Col} className="mb-3">
+                <Form.Label>Adres</Form.Label>
+                <Form.Control as="textarea" rows={2} />
+                </Form.Group>
+              </Row>
               <Row className="row-cols-1 row-cols-md-1">
                 <Form.Group as={Col} className="mb-3">
                 <Form.Label>Şikayet</Form.Label>
                 <Form.Control as="textarea" rows={3} />
-                  <Form.Control.Feedback type="invalid">
-                  </Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-1">
+                <Form.Group as={Col} className="mb-3">
+                <Form.Label>Hikayesi</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-1">
+                <Form.Group as={Col} className="mb-3">
+                <Form.Label>Yapılan tedavi</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-1">
+                <Form.Group as={Col} className="mb-3">
+                <Form.Label>Verilen ilaç</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Row>
+            <Row className="row-cols-1 row-cols-md-1">
+                <Form.Group as={Col} className="mb-3">
+                <Form.Label>Tavsiyeler</Form.Label>
+                <Form.Control as="textarea" rows={3} />
                 </Form.Group>
               </Row>
-            </Col>
+          </Col>
+          
+
+
          </Row>
       </Form>
     </Container>
