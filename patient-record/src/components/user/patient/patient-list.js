bfdcd41ba65c90151d49e7123559fbf6d747bfc5
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-09-13 18:59:16 +0400

    09/13

--- patient-record/src/components/user/patient/patient-list.js
+++ patient-record/src/components/user/patient/patient-list.js
@@ -3,7 +3,8 @@ import DataTable from "react-data-table-component";
 import { Link, useNavigate } from "react-router-dom";
 import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
-import { getPatients } from "../../../api/patience-service";
+import { getPatients, getPatientsByPage } from "../../../api/patience-service";
+import { toast } from "../../../helpers/functions/swal";
 
 const columns = [
     {
@@ -23,15 +24,18 @@ const columns = [
   ];
 
 const PatientList = () => {
-    const [patients, setPatients] = useState([]);
+  const [patients, setPatients] = useState([]);
   const navigate = useNavigate();
-  const [filters, setFilters] = useState({ q: ""})
+  const [sortValue, setSortValue] = useState("id");
+  const [directionValue, setDirectionValue] = useState("DESC");
+  const [paging, setPaging] = useState({});
+  const [filters, setFilters] = useState({ q: "" })
 
 
     const loadData = async () => {
         try {
-          const resp = await getPatients();
-          setPatients(resp.data);
+          // const resp = await getPatients();
+          // setPatients(resp.data);
         } catch (err) {
           console.log(err);
         } finally {
@@ -39,9 +43,38 @@ const PatientList = () => {
     };
     
     useEffect(() => {
-        loadData(0);
-        // eslint-disable-next-line
-    }, []);
+      const timer = setTimeout(() => {
+        getPatientsPage(0);
+      }, 1000);
+      return () => {
+        clearTimeout(timer);
+      }
+    }, [filters,sortValue,directionValue]);
+  
+    useEffect(() => {
+      loadData(0);
+      // eslint-disable-next-line
+  }, []);
+  
+    const getPatientsPage = async (page) => {
+      try {
+        const resp = await getPatientsByPage({
+          q: filters.q,
+          name: filters.categories,
+          lastName: filters.brands,
+          phoneNumber: filters.number,
+          page,
+          sort: sortValue,
+          direction: directionValue
+        });
+        const { content, totalPages, pageable } = resp.data;
+        setPatients(content);
+        setPaging({ totalPages, pageNumber: pageable.pageNumber });
+      } catch (err) {
+        const message = err.response ? err.response.data.message : err;
+        toast(message, "error");
+      }
+    };
   
     const handleFilterChange = (e) => {
       const name = e.target.name;
@@ -62,10 +95,29 @@ const PatientList = () => {
           <InputGroup>
             <Form.Control
               type="search"
-              name="q"
-              value={filters.q}
+              name="name"
+              value={filters.name}
               onChange={handleFilterChange}
-              placeholder="Search"
+              placeholder="Isim"
+              className="ms-2"
+            />
+
+            <Form.Control
+              type="search"
+              name="lastName"
+              value={filters.lastName}
+              onChange={handleFilterChange}
+              placeholder="Soy isim"
+              className="ms-2"
+            />
+            
+            <Form.Control
+              type="search"
+              name="phoneNumber"
+              value={filters.phoneNumber}
+              onChange={handleFilterChange}
+              placeholder="Telefon Numarası"
+              className="ms-2"
             />
             <InputGroup.Text>
               <FaSearch />
