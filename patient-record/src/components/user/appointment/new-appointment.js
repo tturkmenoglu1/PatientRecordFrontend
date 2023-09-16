599d19e4d74f53c8448e8aea9dab3f2010934ef8
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-09-16 21:10:02 +0400

    09/16

--- patient-record/src/components/user/appointment/new-appointment.js
+++ patient-record/src/components/user/appointment/new-appointment.js
@@ -1,10 +1,11 @@
-import { useFormik } from 'formik'
+import { Form, useFormik } from 'formik'
 import React, { useEffect } from 'react'
 import * as Yup from "yup";
 import { addAppointment } from '../../../api/appointment';
 import { toast } from '../../../helpers/functions/swal';
 import { useState } from 'react';
 import { getPatients } from '../../../api/patience-service';
+import { Button, ButtonGroup, Col, Container, Row, Spinner } from 'react-bootstrap';
 
 const NewAppointment = () => {
   const [loading, setLoading] = useState(false);
@@ -20,22 +21,23 @@ const NewAppointment = () => {
   }
 
   const initialValues = {
-    patientId: 0,
-    appointmentDate: "",
-    about: "",
+    patientId: "",
+    // appointmentDate: "",
+    // about: "",
   }
 
-  const validationSchema = {
+  const validationSchema = Yup.object({
     patientId: Yup.number().required("Hasta seçin"),
-    appointmentDate: Yup.string().required("Tarih belirtin"),
-    about: Yup.string().max(2000,"maksimum 2000 karakter"),
-  }
+    // appointmentDate: Yup.string().required("Tarih belirtin"),
+    // about: Yup.string().max(2000,"maksimum 2000 karakter"),
+  });
 
   const onSubmit = async (values) => {
     setLoading(true);
     try {
-      await addAppointment(values);
+      const response = await addAppointment(values);
       toast("Randevu kaydedildi", "success");
+      console.log(response)
     } catch (error) {
       toast(error.response.data.message, "error");
     }finally {
@@ -51,12 +53,52 @@ const NewAppointment = () => {
 
   useEffect(() => {
     loadData();
-  
+
   }, [])
   
 
   return (
-    <div>NewAppointment</div>
+    <Container fluid>
+      <Form noValidate onSubmit={formik.handleSubmit}>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>Hasta Seçin</Form.Label>
+              <Form.Select
+                  name="number"
+                  className="mb-2 mt-2"
+                  {...formik.getFieldProps("patientId")}
+                  isValid={
+                    formik.touched.patientId && !formik.errors.patientId
+                  }
+                  isInvalid={
+                    formik.touched.patientId && !!formik.errors.patientId
+                  }
+              >
+                <option>Hasta</option>
+                {patientIdData.map((option) => {
+                  return (
+                    <option value={option.id} key={option.id}>
+                      {option.firstName + " "+ option.lastName}
+                    </option>
+                  )
+                })} 
+              </Form.Select>
+              <Form.Control.Feedback type="invalid">
+                  {formik.errors.patientId}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Col>
+          <ButtonGroup className="mt-5">
+              <Button
+                variant="primary"
+                type="submit">
+                {loading && <Spinner animation="border" size="sm" />} Create
+              </Button>
+            </ButtonGroup>
+        </Row>
+      </Form>
+    </Container>
   )
 }
 
